30d95659b93277ce25503d6f1e454acfcd7e7f2d
j1ngzoue <workdrive0306@gmail.com>
2022-02-10 13:58:52 +0900

    Add fib

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,32 @@
-fn main() {}
+struct FibIterator {
+    a: usize,
+    b: usize,
+}
+
+impl FibIterator {
+    fn new() -> Self {
+        FibIterator { a: 1, b: 1 }
+    }
+}
+
+impl Iterator for FibIterator {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        let tmp = self.a;
+        self.a = self.b;
+        self.b += tmp;
+        return Some(self.a);
+    }
+}
+
+fn main() {
+    let fib_iter = FibIterator::new();
+    for (i, n) in fib_iter.enumerate() {
+        if i >= 10 {
+            break;
+        }
+        print!("{},", n);
+    }
+
+    println!("");
+}
